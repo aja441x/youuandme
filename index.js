b1080a681e79df9117298d584c808a400ac634f0
aja441x <bxybaby51@gmail.com>
2025-02-06 17:30:36 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('Gracias, sabia que ibas a aceptar mi linda conejita, dame a kaveh.')
+})
